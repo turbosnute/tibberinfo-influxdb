a5f7eb2e8c1d326bbfd2d0a148b6edf5ed294a0b
Patrick Nagel <mail@patrick-nagel.net>
2025-02-04 20:53:08 +0100

    Adjust various files so that they fit together and docker compose works again

--- get.sh
+++ get.sh
@@ -4,22 +4,12 @@ set -aux
 cd $(dirname $0)
 
 INTERVAL_SECONDS=${INTERVAL_SECONDS:-43200}
-CHECK=`echo "${LOAD_HISTORY:-False}" | tr '[:upper:]' '[:lower:]'`
 
-#echo "CHECK: $CHECK"
-
-if [ "$CHECK" = 'true' ]
-then
-  echo "Loading history..."
-  python3 tibberinfo.py
-  echo "History loaded. Now you can run the container without the LOADHISTORY variable..."
-else
-  while :
-  do
-    echo "--- Start Call API"
-    python3 tibberinfo.py
-    RET=$?
-    echo "Sleep ${INTERVAL_SECONDS} seconds"
-    sleep ${INTERVAL_SECONDS}
-  done
-fi
+while :
+do
+  echo "--- Start Call API"
+  python3 tibberinfo.py --verbose
+  RET=$?
+  echo "Sleep ${INTERVAL_SECONDS} seconds"
+  sleep ${INTERVAL_SECONDS}
+done
